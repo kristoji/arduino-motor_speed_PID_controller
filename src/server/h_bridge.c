89e100e3bb5e4f01cc4ac810e680b81501732a6b
kristoj <kristoj@pop-os.localdomain>
2024-05-27 10:35:10 +0200

    refactoring h_bridge.c

--- src/server/h_bridge.c
+++ src/server/h_bridge.c
@@ -60,8 +60,8 @@
 #define M_12 1<<6
 #define M_11 1<<5
 
-
-int main(void){
+void setup()
+{
   UART_init();
   UART_putString((uint8_t*)"\nINIT\n");
 
@@ -78,29 +78,17 @@ int main(void){
 
   // set output
   DDRB |= (M_11 | M_12); 
+}
 
-  short int intensity = 0;
-  while(1){
+void update_PWM(char in)
+{
+    static short int intensity = 0;
 
-    // get "h" or "l" in input
-    char in[1024];
-    int len_in = UART_getString(in); 
-    if (len_in != 3 || (in[0] != 'h' && in[0] != 'l'))
-    {
-      // print len
-      char out[1024];
-      sprintf(out, "len = %d\n", len_in);
-      UART_putString((uint8_t*)out);
-
-      UART_putString((uint8_t*)"Usage: \"h\" for higher speed, \"l\" for lower speed\n");
-      continue;
-    }
-
-    if (in[0] == 'h')
+    if (in == 'h' && intensity+8 <= 255)
     {
       intensity += 8;
     }
-    else
+    else if (in == 'l' && intensity-8 >= -255)
     {
       intensity -= 8;
     }
@@ -115,12 +103,41 @@ int main(void){
       OCR1AL = 0;
       OCR1BL = -intensity;
     }
-    
-    
-    unsigned char out[1024];
-    sprintf(out, "pin11 = %u\npin12 = %u\n", (int) OCR1AL, (int) OCR1BL); 
-    UART_putString((uint8_t*)out);
+}
 
+char get_input()
+{
+  char in[1024];
+  int len_in = UART_getString(in); 
+  
+  while(len_in != 3 || (in[0] != 'h' && in[0] != 'l')) 
+  {
+    UART_putString((uint8_t*)"Usage: \"h\" for higher speed, \"l\" for lower speed\n");
+ 
+    len_in = UART_getString(in);
+  }
+
+  return in[0];
+}
+
+void print_status() 
+{
+  unsigned char out[1024];
+  sprintf(out, "pin11 = %u\npin12 = %u\n", (int) OCR1AL, (int) OCR1BL);
+  UART_putString(out);
+}
+
+int main(void)
+{
+  setup();
+
+  while(1)
+  {
+    char in = get_input();
+
+    update_PWM(in);
+    
+    print_status();
     
     _delay_ms(100);
   }
